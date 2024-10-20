c4630073b3fe90f049e275105fb5f4a01435352f
IT24100781 <IT24100781@MY.SLIIT.LK>
2024-10-20 23:49:50 +0530

    Add files via upload

---
+++ Q2/IT24100781Lab7Q2A.java
@@ -0,0 +1,11 @@
+public class IT24100781Lab7Q2A{
+	public static void main(String[]args){
+		
+		for (int i=1; i<=4; i++){
+			for (int j=1; j<=5; j++){
+				System.out.print("$ ");
+			}
+			System.out.println();
+		}
+	}
+}
